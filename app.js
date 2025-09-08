1920717b523eda59237f7f1fa37c2ca9b8e85942
developer-iko-mike <developer.iko.mike@gmail.com>
2025-09-08 17:13:12 +0200

    E4

--- app.js
+++ app.js
@@ -4,3 +4,11 @@ const sayHello = (name) => {
 }
 
 sayHello("mohammad")
+// console.log(window) // ❌ نمیشه در نود چی اس استفاده کرد
+// document.getElementById("") // ❌ نمیشه در نود چی اس استفاده کرد
+// window.setTimeout(() => { console.log("morning time") } , 1500) // ❌ نمیشه در نود جی اس استفاده کرد
+console.log(global) // بجای ویندو باید از گلوباس استفاده بکنیم
+global.setTimeout(() => { console.log("morning time") } , 1500)
+
+console.log(__dirname) // محلی که فایل app.js وجود دارد رو به ما نشون میده // G:\B\AA-NotesCourse\learnNode
+console.log(__filename) // اسم فایلی که که توش قرار داریم رو نشون میده // G:\B\AA-NotesCourse\learnNode\app.js
