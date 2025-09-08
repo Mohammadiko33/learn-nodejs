0f4790603ccdb2096c7d33b74d05b2ef081e2f97
developer-iko-mike <developer.iko.mike@gmail.com>
2025-09-08 20:38:06 +0200

    E7 : wrrite all text file

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const fs = require("fs")
 
-fs.readFile("./docs/blog.txt" , (err , data) => {
-    if (err) console.log(err);
-    console.log(data.toString()) // so stop the nozi thats life shi red rozzi
+// fs.readFile("./docs/blog.txt" , (err , data) => {
+//     if (err) console.log(err);
+//     console.log(data.toString())
+// })
+
+fs.writeFile("./docs/blog.txt" , "yall nigga las and nas vegas" , () => {
+    console.log("write readed successfully")
 })
