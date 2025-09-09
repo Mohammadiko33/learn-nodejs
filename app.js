821b9a947f299f033ed604a8d9b108674fd4baf5
developer-iko-mike <developer.iko.mike@gmail.com>
2025-09-09 11:02:27 +0200

    E10 : write file with stream and change buffer to utf8

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const fileSystem = require("fs")
 
-const readStream = fileSystem.createReadStream("./blog.txt")
+const readStream = fileSystem.createReadStream("./blog.txt" , {encoding: "utf8"})
+const wrriteStream = fileSystem.createWriteStream("./result.tex")
 
 readStream.on("data" , (buffer) => {
+    // Read stream
     console.log("new buffer")
     console.log(buffer)
+    // wrrite stream
+
+    wrriteStream.write("\n--------------- new buffer ---------------\n")
+    wrriteStream.write(buffer)
 })
