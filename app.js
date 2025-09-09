103c6edd647678335f34b6fd10f9fd28207fedb2
developer-iko-mike <developer.iko.mike@gmail.com>
2025-09-09 19:54:08 +0200

    E26 : install express , send simple hello world

--- app.js
+++ app.js
@@ -1,43 +1,9 @@
-const _ = require("lodash")
-const fs = require("fs");
-const http = require("http");
+const express = require("express")
 
-const server = http.createServer((req, res) => {
-  console.log(_.random(0 , 20))
+const app = express()
 
-  path = "./vue/";
-  switch (req.url) {
-    case "/":
-      path += "index.html";
-      res.statusCode = 200
-      break;
-    case "/about":
-      path += "about.html";
-      res.statusCode = 200
-      break;
-    case "/about-me":
-      res.statusCode = 301
-      res.setHeader("Location" , "/about")
-      res.end()
-      break;
-    default:
-      path += "notFounded.html";
-      res.statusCode = 404
-      break;
-  }
+app.listen(3000)
 
-  res.setHeader("Content-Type", "text/html");
-
-  fs.readFile(path, (err, data) => {
-    if (err) {
-      console.log(err);
-    } else {
-      // res.write(data)
-      res.end(data);
-    }
-  });
-});
-
-server.listen(3000, "localhost", () => {
-  console.log("server created successfully in port 3000");
-});
+app.get("/" , (req , res) => {
+    res.send("Hello World")
+})
