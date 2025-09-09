f6111a866bc4ec790592ed59e8691476a12c9bff
developer-iko-mike <developer.iko.mike@gmail.com>
2025-09-09 08:01:25 +0200

    E9 : part 2 : create folder , create file , wrrite in file , append in file , delete file , delete folder

--- app.js
+++ app.js
@@ -1,29 +1,51 @@
-const fs = require("fs");
+const fs = require("fs").promises;
 
-// fs.readFile("./docs/blog.txt" , (err , data) => {
-//     if (err) console.log(err);
-//     console.log(data.toString())
-// })
+async function createProject_removeProject({
+  fileName,
+  folderName,
+  fileType,
+  fileData,
+  fileMoreData = "",
+}) {
+  const address = `./${folderName}/${fileName}.${fileType}`;
+  try {
+    // ساخت پوشه
+    await fs.mkdir(`./${folderName}`);
+    console.log("folder created successfully");
 
-// fs.writeFile("./docs/blog.txt" , "yall nigga las and nas vegas" , () => {
-//     console.log("write readed successfully")
-// })
+    await new Promise((resolve) => setTimeout(resolve, 1000));
 
-// fs.appendFile("./docs/blog.txt" , "follow bas follow bag" , () => {
-//     console.log("append wrrite file successfully")
-// })
+    // نوشتن فایل
+    await fs.writeFile(address, fileData);
+    console.log("file written successfully");
 
-// fs.mkdir("./docs2", (err) => {
-//   if (err) console.log(err);
-//   console.log("folder created");
-// });
+    if (fileMoreData) {
+      await new Promise((resolve) => setTimeout(resolve, 2000));
+      await fs.appendFile(address, fileMoreData);
+      console.log("file appended successfully");
+    }
 
-// fs.rmdir(".qodo", () => {
-//   if (err) console.log(err);
-//   console.log("folder deleted");
-// });
+    await new Promise((resolve) => setTimeout(resolve, 3000));
 
-fs.unlink("./temp.test.tsx" , (err) => {
-  if (err) console.log(err);
-  console.log("delete file successfully")
-})
+    // حذف فایل
+    await fs.unlink(address);
+    console.log("file deleted successfully");
+
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+
+    // حذف پوشه
+    await fs.rmdir(`./${folderName}`);
+    console.log("folder deleted successfully");
+
+    console.log("end");
+  } catch (err) {
+    console.log("Error:", err.message);
+  }
+}
+
+createProject_removeProject({
+  folderName: "kharazmi",
+  fileName: "khirzade",
+  fileType: "txt",
+  fileData: `hello khirzade`,
+});
