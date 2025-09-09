b55d8ec77595974514c3fbbcaf14d73722990554
developer-iko-mike <developer.iko.mike@gmail.com>
2025-09-09 14:45:53 +0200

    E16 : first response

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 const http = require("http")
 
-const server = http.createServer((req , res) => console.log("request made"))
+const server = http.createServer((req , res) => {
+    console.log(req.url)
+    console.log(req.method)
 
-server.listen(3000 , () => {
+    res.setHeader("Content-Type" , "text/html")
+    
+    // res.write("<head> <script src='./script.js' defer></script> </head>")
+    res.write("<h1>Hello i'm in course node js</h1>")
+    res.write("<p>this is episode 16</p>")
+})
+
+server.listen(3000 , "localhost" , () => {
     console.log("server created successfully in port 3000")
 })
