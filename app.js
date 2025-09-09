c70e42ab38a3f09a9e4c341e5429ac6e3a329104
developer-iko-mike <developer.iko.mike@gmail.com>
2025-09-09 10:54:18 +0200

    E10 : streams

--- app.js
+++ app.js
@@ -1,56 +1,8 @@
-const fs = require("fs").promises;
+const fileSystem = require("fs")
 
-async function createProject_removeProject({
-  fileName,
-  folderName,
-  fileType,
-  fileData,
-  fileMoreData = "",
-}) {
-  const address = `./${folderName}/${fileName}.${fileType}`;
-  try {
-    // ساخت پوشه
-    await fs.mkdir(`./${folderName}`);
-    console.log("folder created successfully");
+const readStream = fileSystem.createReadStream("./blog.txt")
 
-    await new Promise((resolve) => setTimeout(resolve, 1000));
-
-    // نوشتن فایل
-    await fs.writeFile(address, fileData);
-    console.log("file written successfully");
-
-    if (fileMoreData) {
-      await new Promise((resolve) => setTimeout(resolve, 2000));
-      await fs.appendFile(address, fileMoreData);
-      console.log("file appended successfully");
-    }
-
-    await new Promise((resolve) => setTimeout(resolve, 1000));
-
-    // خواندن فایل قبل از حذف
-    const content = await fs.readFile(address, "utf-8");
-    console.log("File content before delete:\n", content);
-
-    await new Promise((resolve) => setTimeout(resolve, 3000));
-    // حذف فایل
-    await fs.unlink(address);
-    console.log("file deleted successfully");
-
-    await new Promise((resolve) => setTimeout(resolve, 1000));
-
-    // حذف پوشه
-    await fs.rmdir(`./${folderName}`);
-    console.log("folder deleted successfully");
-
-    console.log("end");
-  } catch (err) {
-    console.log("Error:", err.message);
-  }
-}
-
-createProject_removeProject({
-  folderName: "kharazmi",
-  fileName: "khirzade",
-  fileType: "txt",
-  fileData: `hello khirzade`,
-});
+readStream.on("data" , (buffer) => {
+    console.log("new buffer")
+    console.log(buffer)
+})
