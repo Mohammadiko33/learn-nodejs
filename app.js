466a64da5cbb8adfcb38a74aa3674406c884a8c5
developer-iko-mike <developer.iko.mike@gmail.com>
2025-09-10 15:44:19 +0200

    E32 : make project component base

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const express = require("express");
 
+const blogs = [
+  {title: "title 1" , caption: "lorem ipsum is a fake title 1"},
+  {title: "title 2" , caption: "lorem ipsum is a fake title 2"},
+  {title: "title 3" , caption: "lorem ipsum is a fake title 3"}
+]
+
 const app = express();
 
 app.set("view engine", "ejs");
@@ -7,17 +13,17 @@ app.set("view engine", "ejs");
 app.listen(3000);
 
 app.get("/", (req, res) => {
-  res.render("index" , {titleInApp: "home"})
+  res.render("index" , {titleInApp: "home", pageTitle: "home" , blogs})
 });
 
 app.get("/about", (req, res) => {
-   res.render("about" , { titleInApp: "about" })
+   res.render("about" , { titleInApp: "about", pageTitle: "about" })
 });
 
 app.get("/blog/new-blog", (req, res) => {
-  res.render("createBlog" , { titleInApp: "create a new blog" })
+  res.render("createBlog" , { titleInApp: "create a new blog", pageTitle: "create a new blog" })
 });
 
 app.use((req , res) => {
-  res.status(404).render("notFounded" , { titleInApp: "404" })
+  res.status(404).render("notFounded" , { titleInApp: "404", pageTitle: "404" })
 })
