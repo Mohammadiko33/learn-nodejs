4f1b94e1e860ab03c4030da7dfb56728f27f68bc
developer-iko-mike <developer.iko.mike@gmail.com>
2025-09-10 11:59:15 +0200

    E31 : part 2 : catch moteghayer  in app.js and show in html or ejs files

--- app.js
+++ app.js
@@ -7,21 +7,17 @@ app.set("view engine", "ejs");
 app.listen(3000);
 
 app.get("/", (req, res) => {
-  res.render("index")
+  res.render("index" , {titleInApp: "home"})
 });
 
 app.get("/about", (req, res) => {
-   res.render("about")
-});
-
-app.get("/about-us", (req, res) => {
-  res.redirect("/about")
+   res.render("about" , { titleInApp: "about" })
 });
 
 app.get("/blog/new-blog", (req, res) => {
-  res.render("createBlog")
+  res.render("createBlog" , { titleInApp: "create a new blog" })
 });
 
 app.use((req , res) => {
-  res.status(404).render("notFounded")
+  res.status(404).render("notFounded" , { titleInApp: "404" })
 })
