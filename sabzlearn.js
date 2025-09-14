eb8367527afe6b67455e2c678b74b391a18feb81
developer-iko-mike <developer.iko.mike@gmail.com>
2025-09-14 17:45:32 +0200

    os

--- sabzlearn.js
+++ sabzlearn.js
@@ -1,11 +1,20 @@
-const path = require("path");
+const os = require("os")
 
-const modelsPathWithModule = path.join(__dirname, "/userProfile");
-const modelsPathWithoutModule = `${__dirname}/userProfile`;
-console.log("modelsPathWithModule : " + modelsPathWithModule);
-console.log("modelsPathWithoutModule : " + modelsPathWithoutModule);
+console.log(os.cpus())
+console.log(os.arch())
+console.log(Math.floor(os.totalmem() / 1_000_000)) // byte
+console.log(Math.floor(os.freemem() / 1_000_000)) // byte
 
-const modelsPathWithModule2 = path.join(__dirname, "../../public");
-const modelsPathWithoutModule2 = `${__dirname}/../../public`;
-console.log("modelsPathWithModule2 : " + modelsPathWithModule2);
-console.log("modelsPathWithoutModule2 : " + modelsPathWithoutModule2);
+// ---------
+
+// const path = require("path");
+
+// const modelsPathWithModule = path.join(__dirname, "/userProfile");
+// const modelsPathWithoutModule = `${__dirname}/userProfile`;
+// console.log("modelsPathWithModule : " + modelsPathWithModule);
+// console.log("modelsPathWithoutModule : " + modelsPathWithoutModule);
+
+// const modelsPathWithModule2 = path.join(__dirname, "../../public");
+// const modelsPathWithoutModule2 = `${__dirname}/../../public`;
+// console.log("modelsPathWithModule2 : " + modelsPathWithModule2);
+// console.log("modelsPathWithoutModule2 : " + modelsPathWithoutModule2);
