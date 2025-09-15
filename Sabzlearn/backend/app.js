432ae8a421c9b693316c080901ff15babe58a394
developer-iko-mike <developer.iko.mike@gmail.com>
2025-09-15 20:45:57 +0200

    fix response to html bug

--- Sabzlearn/backend/app.js
+++ Sabzlearn/backend/app.js
@@ -2,26 +2,48 @@ const http = require("http");
 const url = require("url");
 
 const users = [
-  { id: 1, email: "mohammad@gmail.com", password: 123123123 },
-  { id: 2, email: "reza@gmail.com", password: 456456456 },
-  { id: 3, email: "amir@gmail.com", password: 789789789 },
+  { id: 1, email: "mohammad@gmail.com", password: "123123" },
+  { id: 2, email: "reza@gmail.com", password: "456456456" },
+  { id: 3, email: "amir@gmail.com", password: "789789789" },
 ];
 
 http
   .createServer((req, res) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+    if (req.method === "OPTIONS") {
+      res.writeHead(204);
+      res.end();
+      return;
+    }
+
     console.log(req.url);
     const myUrl = new URL(req.url, `http://${req.headers.host}`);
     const usermail = myUrl.searchParams.get("email");
     const password = myUrl.searchParams.get("pass");
-    console.log(usermail, password);
-    const findDataFromEmail = users.find(user => user.email === usermail)
-    console.log(findDataFromEmail)
-    if (findDataFromEmail){
-      const mainUser = users.find(user => user.password === password)
-      console.log(mainUser)
-      if (mainUser){
-        res.sendDate(mainUser)
+
+    const findDataFromEmail = users.find((user) => user.email === usermail);
+    if (findDataFromEmail) {
+      const mainUser = users.find((user) => user.password === password);
+      if (mainUser) {
+        res.write(JSON.stringify({ success: true, user: mainUser }));
+      } else {
+        res.write(
+          JSON.stringify({
+            success: false,
+            message: "password is wrong, are you forgot your password ?",
+          })
+        );
       }
+    } else {
+      res.write(
+        JSON.stringify({
+          success: false,
+          message: "Email Not founded You need Register first",
+        })
+      );
     }
     res.end();
   })
