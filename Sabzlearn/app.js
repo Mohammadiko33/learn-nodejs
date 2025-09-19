c884b080c993d2ef75d686dbd30e3d939390d057
developer-iko-mike <developer.iko.mike@gmail.com>
2025-09-19 11:28:56 +0200

    POST method in node js

--- Sabzlearn/app.js
+++ Sabzlearn/app.js
@@ -12,5 +12,20 @@ db.connect((err) => {
     console.log(err);
   } else {
     console.log(`🔥 app.js:14 => connect database with successfully`);
+    const newQuery = `INSERT INTO courses VALUES (
+    NULL,
+    "دوره متخصص پایتون",
+    "کپشن تستی برای دوره",
+    2000000,
+    "لیچ ماب"
+  )
+    `;
+    db.query(newQuery, (err, data) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      console.log(data);
+    });
   }
 });
