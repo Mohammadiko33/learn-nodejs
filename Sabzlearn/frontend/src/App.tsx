616fad6c51c67c61b096aa551e2e828a4924eb23
developer-iko-mike <developer.iko.mike@gmail.com>
2025-09-20 06:31:41 +0200

    add front end

--- Sabzlearn/frontend/src/App.tsx
+++ Sabzlearn/frontend/src/App.tsx
@@ -17,7 +17,7 @@ const App = () => {
     gmail: "",
     password: ""
   });
-  const [loading , setLoading] = useState<boolean>(false)
+  const [loading, setLoading] = useState<boolean>(false)
 
   const handleChange = (e: Che) => {
     const { name, value } = e.target;
@@ -30,36 +30,36 @@ const App = () => {
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
     if (form.username.length < 3) {
-      toast.error("username input must be 3 chrackter", { position: "bottom-left" })
+      toast.error("username input must be 3 chrackter")
       return;
     }
     if (form.gmail.length < 13) {
-      toast.error("gmail input must be 13 chrackter", { position: "bottom-left" })
+      toast.error("gmail input must be 13 chrackter")
       return;
     }
     if (form.password.length < 6) {
-      toast.error("password input must be 6 chrackter", { position: "bottom-left" })
+      toast.error("password input must be 6 chrackter")
       return;
     }
     setLoading(true)
     try {
-      const res = await fetch("http://localhost:3000/api/users/new-user" , {
-        method: "POST",
-        headers: { "Content-type": "application/json" },
-        body: JSON.stringify(form)
-      })
-      const parseRes = await res.json()
+    const res = await fetch("http://localhost:3000/")
+    console.log(res)
+    const parseRes = await res.json()
+    console.log(parseRes)
+    toast.success(parseRes.message)
+    setLoading(false)
       if (!parseRes.message){
       console.log(parseRes)
       console.log(res.status)
       if (res.status === 201){
-        toast.success("submited", { position: "bottom-left" })
+        toast.success("submited")
       }
     } else {
-      toast.warn(parseRes.message , { position: "bottom-left" })
+      toast.warn(parseRes.message)
     }
     } catch (err) {
-      toast.error("you have error check console" , { position: "bottom-left" })
+      toast.error("you have error check console")
       console.log(err)
     } finally {
       setLoading(false)
@@ -69,7 +69,12 @@ const App = () => {
   const inputClass = "w-full px-4 py-2 border border-sky-400 rounded-lg";
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-black">
-      <ToastContainer />
+      <ToastContainer
+        toastClassName="my-toast-font"  // کلاس CSS سفارشی برای همه توست‌ها
+        position="top-left"
+        draggable
+      />
+
       <h1 className="text-3xl font-extrabold mb-6 text-white">Register</h1>
       <form
         onSubmit={handleSubmit}
@@ -111,9 +116,9 @@ const App = () => {
 
         <div className="flex justify-center gap-2.5 text-sm">
           <p>Already have an account?</p>
-          <Link to="/login" className="text-sky-400 hover:underline">
+          <button onClick={() => window.location.reload()} className="text-sky-400 hover:underline">
             Login
-          </Link>
+          </button>
         </div>
 
         <button
